a6629675d4799b99fbc9ce814424c1de8c4d967d
fyacoubi <>
2023-03-02 19:30:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fractions import Fraction
+
+#given a fraction, represent it in egyptian format
+
+input = input("Input Fraction Here: ")
+current = Fraction(input)#value left over of input after every iteration
+i = 2
+series = Fraction(1,i) #1/2, 1/3, ... , 1/n
+result = []
+while 1:
+	if series == current:
+		result.append(series)
+		break
+	if current > series:
+		result.append(series)
+		current = current - series
+		i += 1
+		series = Fraction(1,i)
+	else:
+		i+=1
+		series = Fraction(1,i)
+
+print(result)
+	
+	
+		
+		
+		
+		
+	
+
+
+
